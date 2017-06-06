af0bfef12ffd27811d295d29c35eb78e21917ba9
Master76 <15811297278@163.com>
2017-06-06 17:10:10 +0800

    move common method to this filse

---
+++ src/utils.py
@@ -0,0 +1,37 @@
+import random
+
+def run(agent, env, show=False):
+    s = env.reset()
+    R = 0
+
+    while True:
+        if show:
+            env.render()
+
+        a = agent.act(s)
+        s_, r, done, info = env.step(a)
+
+        if done:  # terminal state
+            s_ = None
+
+        agent.observe((s, a, r, s_))
+
+        if r is not 0:
+            if r > 0 or random.random() > agent.epsilon - 0.1:
+                agent.replay(env.moves)
+
+        s = s_
+        R += r
+
+        if done:
+            print(info)
+            return R
+
+def save_model(name='snake.model'):
+    agent.brain.model.save(name)
+
+def log(text):
+    print(text)
+    with open('log.txt', mode='a') as fs:
+        fs.write(text)
+        fs.write('\n')
