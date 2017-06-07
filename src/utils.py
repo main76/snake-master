396d3243d25f05bc6058cea9d003c1cf8f04b8b4
Master76 <15811297278@163.com>
2017-06-07 16:26:30 +0800

    adjust log format

--- src/utils.py
+++ src/utils.py
@@ -22,7 +22,7 @@ def start(agent, out_path, TOTAL_EPISODES=640000, BATCH_SIZE_BASELINE=1000, show
         score_sum += score
         episode_number += 1
         if episode_number % BATCH_SIZE_BASELINE == 0:
-            log('Episode: %d, Average reward and score for episode: %f, %f.' %
+            log('Episode: %d, Average reward and score for episode: %f, %.3f.' %
                 (episode_number, reward_sum / BATCH_SIZE_BASELINE, score_sum / BATCH_SIZE_BASELINE))
             reward_sum = 0
             score_sum = 0
