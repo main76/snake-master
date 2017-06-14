14314485b8b41b62d10e92b20b3410900202a886
Master76 <15811297278@163.com>
2017-06-14 10:57:24 +0800

    procceed training

--- src/load.py
+++ src/load.py
@@ -4,8 +4,8 @@ import os
 
 abs_path = os.path.dirname(os.path.abspath(__file__))
 out_path = os.path.join(abs_path, '..', 'output')
-model_path = os.path.join(abs_path, '..', 'pretrained', 'snake.10x10_2.56m.model')
+model_path = os.path.join(abs_path, '..', 'pretrained', 'snake.10x10_0.64m.model')
 
-SET_MIN_EPSILON(0)
+# SET_MIN_EPSILON(0) still keep curious
 agent = Agent(INPUT_SHAPE, ACTION_COUNT, 640000, model_path, 0.2)
-start(agent, out_path, show=False)
+start(agent, out_path, TOTAL_EPISODES=2560000 show=False)
